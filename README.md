1d59a65b181c2a6c369c3dc60bb1c414439b20db
Eryk Wójcik <drumpixtv@gmail.com>
2025-01-09 13:29:28 +0100

    updated TODOs

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 
 ## Data:
 Reddit:
-- [ ] r/dndmaps
-- [ ] r/mapmaking
-- [ ] r/DnDMapsSubmissions
-- [ ] r/FantasyMaps
-- [ ] r/imaginarymaps
+- [x] r/dndmaps
+- [x] r/mapmaking
+- [x] r/DnDMapsSubmissions
+- [x] r/FantasyMaps
+- [x] r/imaginarymaps
 Other:
 - [ ] [Fantasy maps](https://dnd-maps.com/page/3/)
 
 ## TODO:
-- [ ] Zbadanie różnych modeli dyfuzyjnych
-- [ ] Fine tuning modelu na danych z reddita
+- [x] Zbadanie różnych modeli dyfuzyjnych
+- [x] Fine tuning modelu na danych z reddita
 - [ ] Uwzględnienie danych z rzeczywistych map na wygenerowanej mapie
 - [ ] Testowanie generowania opisu rzeczywistej mapy, aby lepiej nakierowywać dyfuzję na to jaki teren jest na mapie.
-- [ ] Testowanie różnych stylów generowanych map.
+- [x] Testowanie różnych stylów generowanych map.
 - [ ] Sprawić aby mapy były jak najbardziej użyteczne pod DnD.
 - [ ] Demo Streamlit    
--- src/flux_infer.py
+++ src/flux_infer.py
@@ -12,8 +12,8 @@ model_config = Flux1.from_alias("dev").model_config
 flux = Flux1(
     model_config=model_config,
     quantize=8,
-    # lora_paths=["lora/loras_dev.safetensors"],
-    # lora_scales=[1.0],
+    lora_paths=["loras/lora_dev_10000.safetensors"],
+    lora_scales=[1.0],
 )
 prompt = "Create a detailed fantasy map for a Dungeons & Dragons (D&D) campaign, featuring majestic mountains rising from an island surrounded by ethereal clouds. The landscape should showcase vibrant shades of green representing lush forests and fields, as well as deep blue waters that encircle the island. The map should be depicted from a top-down perspective, allowing for a clear view of the terrain, topography, and any significant landmarks that may be present, such as rivers, valleys, or magical sites."
 
@@ -24,7 +24,44 @@ strengths = [0.3, 0.325, 0.35, 0.375, 0.4, 0.425, 0.45, 0.475, 0.5]
 fig, axs = plt.subplots(1, len(strengths), figsize=(30, 4))
 
 for i, strength in enumerate(strengths):
-    print(f"{i}/{len(strengths)}: strength {strength}")
+    print(f"{i}/{len(strengths)-1}: strength {strength}")
+    seed = random.randint(0, 1000000000)
+    image = flux.generate_image(
+        seed=seed,
+        prompt=prompt,
+        config=Config(
+            num_inference_steps=20,
+            height=1024,
+            width=1024,
+            init_image_path=image_path,
+            init_image_strength=strength,
+            guidance=2.0
+        )
+    )   
+    # save the tmp image
+    image.save(f"data/maps/flux_lora_dev/map_{i}.png")
+    axs[i].imshow(image.image)
+    axs[i].set_title(f"Strength: {strength}")
+    axs[i].axis('off')
+
+plt.tight_layout()
+plt.savefig("data/maps/flux_lora_dev/map_forest_10000_2.png", dpi=300)
+
+
+
+
+
+
+# Then create Flux1 with the config and LoRA parameters
+flux = Flux1(
+    model_config=model_config,
+    quantize=8,
+)
+
+fig, axs = plt.subplots(1, len(strengths), figsize=(30, 4))
+
+for i, strength in enumerate(strengths):
+    print(f"{i}/{len(strengths)-1}: strength {strength}")
     seed = random.randint(0, 1000000000)
     image = flux.generate_image(
         seed=seed,
@@ -45,16 +82,4 @@ for i, strength in enumerate(strengths):
     axs[i].axis('off')
 
 plt.tight_layout()
-plt.savefig("data/maps/flux_base_dev/map_forest.png", dpi=300)
-
-
-
-
-# mflux-generate \
-#    --prompt "fantasy map dnd mountains island clouds green and blue top down view" \
-#    --model schnell \
-#    --steps 4 \
-#    --seed 5 \
-#    --lora-paths "lora/lora_schnell.safetensors" \
-#    --lora-scales 1.0 \
-#    -q 8
+plt.savefig("data/maps/flux_base_dev/map_forest_2.png", dpi=300)
